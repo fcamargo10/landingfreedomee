b7ef1487a98dc24d8b0a7c93fc597b362276a4ed
Filipe Camargo <filipecamargo10@gmail.com>
2021-02-09 16:43:56 -0300

    landing

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>FredomeE - #made4better</title>
+
+</head>
+
+<body>
+
+
+    <div class="container-fluid">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="row main text-center">
+                    <div class="col-sm-12 col-md-5">
+                        <img src="/assets/img/logo.png" class="mx-auto d-block img-fluid">
+                        <img src="/assets/img/globe.png" class="mx-auto d-block img-fluid">
+                        <span class="dvleft d-none d-lg-block"></span>
+                    </div>
+                    <div class="col-sm-12 col-md-7 p-2">
+                        <p class="site-construcao poppins-light text-uppercase">Site em construção</p>
+                        <p class="lulo-clean text2">Unimos consumidores conscientes a empreendedores éticos!</p>
+                        <p class="poppins-light text3">Estamos contruindo uma plataforma diferente de tudo que você já viu.<br>Um novo ecossistema de impacto positivo onde você fará diferente encontrando seus iguais.</p>
+
+                        <div style="max-width: 500px; margin:50px auto auto auto;">
+                            <p class="poppins-light text-left">Junte-se a nós!<br>Deixe seus dados e receba o aviso do nosso lançamento.</p>
+
+                            <form action="">
+                                <div class="form-group">
+                                    <input type="text" class="form-control bgroxo" style="max-width: 460px;" placeholder="nome:">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control mt-2 bgroxo" style="max-width: 460px;" placeholder="email:">
+                                </div>
+                                <div class="form-actions">
+                                    <button type="submit" class="btn btn-dark float-left">Enviar</button>
+                                </div>
+
+
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
