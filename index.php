13557f5bcfd71d40b15f4c8e4a1a07c8d9cadca4
Filipe Camargo <filipecamargo10@gmail.com>
2021-02-09 16:46:35 -0300

    favicon

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>FredomeE - #made4better</title>
+    <meta name="description" content="FreedomeE social network.">
+    <meta name="keywords" content="social, wowonder, social site">
+    <link rel="shortcut icon" type="image/png" href="/assets/img/icon.png"/>
 
 </head>
 
